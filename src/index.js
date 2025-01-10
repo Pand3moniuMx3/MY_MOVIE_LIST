77a75b98f2f46c6aa3ae71470c0f613ec0550820
Mateusz <srgmedia.agency@gmail.com>
2025-01-10 11:20:59 +0000

    v1.1 - deleted unused components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import StarRating from "./components/StarRating";
-import TextExpander from "./components/TextExpander";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
